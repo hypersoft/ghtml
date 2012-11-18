da205b475051f456d141c88f1beff882ec34d060
Triston J. Taylor <pc.wiz.tt@gmail.com>
2012-11-18 05:52:00 -0600

    Added hide window js
    Added: include/webview/js/hide.c

---
+++ include/webview/js/hide.c
@@ -0,0 +1,47 @@
+/*
+	This file is part of ghtml.
+
+    ghtml is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ghtml is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ghtml.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+SeedValue ghtml_webview_js_getenv (SeedContext ctx, SeedObject function, SeedObject thisObject, size_t argumentCount, SeedValue arguments[], SeedException * exception) {
+
+	if (argumentCount) {
+		seed_make_exception (ctx, exception, GHTML_JS_INVALID_PARAMS,
+			"getenv expected 1 argument, got %zd", argumentCount
+		);  return seed_make_null (ctx);
+	}
+
+	gchar * var = seed_value_to_string(ctx, arguments[0], exception);
+	gchar * val = (void * )g_getenv(var);
+	
+	g_free(var);
+
+	if (! val) {
+		return seed_make_undefined(ctx);
+	}
+
+	void * jsTemp;
+
+	SeedValue result = (SeedValue)JSValueMakeString(ctx,
+		jsTemp = JSStringCreateWithUTF8CString(val)
+	);  JSStringRelease(jsTemp);
+
+	g_free(val);
+ 
+	return result;
+
+}
+
